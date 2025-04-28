64ec86cec8de445756dcd8b2e4b6b57eda558729
T-Tkhru <ttkirua1@yahoo.co.jp>
2025-04-28 16:34:48 +0900

    [FEAT]スタート画面をちょい追加

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -5,15 +5,11 @@ public class StartGame : MonoBehaviour
 {
     public void easyStart()
     {
-        CutNumManager.Instance.cutLimitTop = 4;
-        CutNumManager.Instance.cutLimitSide = 1;
         SceneManager.LoadScene("MainGame");
     }
 
-    public void hardStart()
+    public void rule()
     {
-        CutNumManager.Instance.cutLimitTop = 6;
-        CutNumManager.Instance.cutLimitSide = 2;
-        SceneManager.LoadScene("MainGame");
+        
     }
 }
